22725024cf89268146aa7890570b4f0ca5eb3818
kleidi <kleidib@gmail.com>
2021-10-24 19:18:00 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -6,20 +6,22 @@ import socket
 tcp_ip = '127.0.0.1'
 port = 5005
 
+#variables used to track speed, direction, and if packet is ready to be sent
 speed = 0
 direction = ""
 full = False
 
+#initializing socket for transferring data at localhost with port 5005
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((tcp_ip, port))
 
 #setting up requirements for pygame capture window
 pygame.init()
-screen = pygame.display.set_mode((100, 100))
-pygame.display.set_caption('Client')
+screen = pygame.display.set_mode((250, 250))
+pygame.display.set_caption('Client Keyboard Capture')
 pygame.mouse.set_visible(1)
 
-
+#loop for pygame to collect input
 active = True
 while active:
     for event in pygame.event.get():
@@ -30,16 +32,24 @@ while active:
             #speed control
             if event.key == pygame.K_0:
                 speed = 0
+                print("Speed set to:", speed)
             if event.key == pygame.K_1:
                 speed = 1
+                print("Speed set to:", speed)
             if event.key == pygame.K_2:
                 speed = 2
+                print("Speed set to:", speed)
             if event.key == pygame.K_3:
                 speed = 3
+                print("Speed set to:", speed)
             if event.key == pygame.K_4:
                 speed = 4
+                print("Speed set to:", speed)
             if event.key == pygame.K_5:
                 speed = 5
+                print("Speed set to:", speed)
+
+            
             
             #wasd keys
             if event.key == pygame.K_w:
@@ -55,6 +65,7 @@ while active:
                 direction = "right"
                 full = True
             
+            #if packet is full/ready, send and print what was sent
             if full == True:
                 packet = direction + "," + str(speed)
                 print("Sent: " + packet)
--- server.py
+++ server.py
@@ -6,18 +6,22 @@ s.bind(('', port))
 s.listen(10)
 
 while 1:
+    #connecting to client
     client_socket, address = s.accept()
     print("Client connected!")
+
+    #recieving packet and decoding into string for analysis
     packet = client_socket.recv(1024)
     message = packet.decode('utf-8')
     direction = message.partition(",")[0]
     speed = message.partition(",")[2]
 
+    #output from server/rover
     if direction == "straight":
-        print("[f" + speed + "][f"+ speed + "][f"+ speed +"][f"+ speed+ "]")
+        print("[f" + speed + "][f" + speed + "][f" + speed + "][f" + speed + "]")
     if direction == "left":
-        print("[f" + speed + "][f" + speed + "][f" + speed + "][f" + speed + "]")
+        print("[r" + speed + "][r" + speed + "][f" + speed + "][f" + speed + "]")
     if direction == "right":
-        print("[f" + speed + "][f" + speed + "][f" + speed + "][f" + speed + "]")
+        print("[f" + speed + "][f" + speed + "][r" + speed + "][r" + speed + "]")
     if direction == "back":
-        print("[f" + speed + "][f" + speed + "][f" + speed + "][f" + speed + "]")
+        print("[r" + speed + "][r" + speed + "][r" + speed + "][r" + speed + "]")
